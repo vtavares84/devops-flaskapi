7285e880827e0bac763b920b1f2c45aa8e56ab09
Vinicius Tavares <vtavares@outlook.com>
2025-01-02 08:46:10 -0300

    feat: add function inser

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
@@ -14,6 +14,39 @@ app.config['MONGODB_SETTINGS'] = {
 }
 db = MongoEngine(app)
 
+
+
+_user_parser = reqparse.RequestParser()
+_user_parser.add_argument(
+    'nome',
+    type=str,
+    required=True,
+    help="This field cannot be blank."
+)
+_user_parser.add_argument(
+    'sobrenome',
+    type=str,
+    required=True,
+    help="This field cannot be blank."
+)
+_user_parser.add_argument(
+    'cpf',
+    type=str,
+    required=True,
+    help="This field cannot be blank."
+)
+_user_parser.add_argument(
+    'email',
+    type=str,
+    required=True,
+    help="This field cannot be blank."
+)
+_user_parser.add_argument(
+    'dt_nascimento',
+    type=str,
+    required=True,
+    help="This field cannot be blank."
+)
 
 class UserModel(db.Document):
     cpf = db.StringField(required=True, unique=True)
@@ -30,7 +63,9 @@ class Users(Resource):
 
 class User(Resource):
     def post(self):
-        return {'message': 'A User'}
+        data = _user_parser.parse_args()
+        UserModel(**data).save()
+        
 
     def get(self, cpf):
         return {"message": cpf}
